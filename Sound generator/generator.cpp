e63279f31fb7b4847f97c9b1e31dc0946946ac37
Dmitrij_Zmejkov <120366668+DmitrijZmejkov@users.noreply.github.com>
2025-07-26 21:00:31 +0300

    Update generator.cpp
    libraries

--- Sound generator/generator.cpp
+++ Sound generator/generator.cpp
@@ -1,1 +1,16 @@
+// 
 
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <portaudio.h>
+#define M_PI 3.14159265358979323846
+
+
+const int SAMPLE_RATE = 44100;
+const int FRAMES_PER_BUFFER = 64;
+
+struct WaveData {
+    std::vector<float> samples;
+    size_t position;
+};
